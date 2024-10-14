01602e35a29491e5e3e3a1be4f47c05c33dc27dc
Abdul Moeed <moeedrajpoot59@gmail.com>
2024-10-13 18:23:52 -0700

    Updated

--- Pre Processing/code_with_Libraries.py
+++ Pre Processing/code_with_Libraries.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import numpy as np 
+print("hello")
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
